3c57ba4ce7892bce2a21945fb00bb037b65027a1
Reuben <rjkoppy@gmail.com>
2022-07-05 16:20:39 -0400

    Generate random dungeon and place player in a room

--- src/map.rs
+++ src/map.rs
@@ -28,10 +28,10 @@ impl Map {
                 let idx = map_idx(x, y);
                 match self.tiles[idx] {
                     TileType::Floor => {
-                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                        ctx.set(x, y, BLACK, BLACK, to_cp437('.'));
                     }
                     TileType::Wall => {
-                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+                        ctx.set(x, y, LIGHT_STEEL, BLACK, to_cp437('#'));
                     }
                 }
             }
