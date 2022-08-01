19aa6fff336335a7148719675f3f54f543080c96
Reuben <rjkoppy@gmail.com>
2022-07-31 20:39:53 -0400

    Update FOV on player move

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -11,6 +11,14 @@ pub fn movement(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
 ) {
+    if let Ok(entry) = ecs.entry_ref(want_move.entity) {
+        if let Ok(fov) = entry.get_component::<FieldOfView>() {
+            commands.add_component(want_move.entity, fov.clone_dirty());
+        }
+        if entry.get_component::<Player>().is_ok() {
+            camera.on_player_move(want_move.destination);
+        }
+    }
     if map.can_enter_tile(want_move.destination) {
         commands.add_component(want_move.entity, want_move.destination);
 
